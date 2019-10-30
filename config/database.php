234fdce635d2296fab0a764bde4cbfd8e87f61ad
yndj <46798345+yndj@users.noreply.github.com>
2019-10-31 06:47:46 +0800

    Revert "Merge branch 'master' of https://github.com/yndj/rhaphp"
    This reverts commit f9f95ee35b27387b3d448a5d88503a2449f4b076, reversing
    changes made to e5d0f7cc8b954e2c536cee0a7fc838091f1bd57b.

--- config/database.php
+++ config/database.php
@@ -15,11 +15,11 @@ return [
 // 服务器地址
 'hostname'        => 'localhost',
 // 数据库名
-'database'        => 'juclnkth',
+'database'        => 'rhaphpv3',
 // 用户名
-'username'        => 'juclnkth',
+'username'        => 'root',
 // 密码
-'password'        => 'uk52Tc20sM',
+'password'        => 'root',
 // 端口
 'hostport'        => '3306',
 // 连接dsn
